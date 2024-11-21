533c4adcf3c8f8f9e302f227bd73d663deef385d
nichoOtero <nichonaks@gmail.com>
2024-11-20 22:47:01 -0600

    props-simpsons-website-lab

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
       <section className="blog-items team-cards">
           <div className="inner-wrapper">
             <div className="blog-container">
-              <ProfileCard pic={homer} />
-              <ProfileCard pic={lisa} />
-              <ProfileCard />
-              <ProfileCard />
-              <ProfileCard pic={bart} />
+              <ProfileCard pic={homer} name={'Homer Simpson'} desc={'Loves his family as much as donuts.'} email={'homer@simpson.com'}/>
+              <ProfileCard pic={lisa} name={'Lisa Simpson'} desc={'Amazing saxophone player.'} email={'lisa@harvard.edu'} />
+              <ProfileCard pic={maggie} name={'Maggie Simpson'} desc={'Maggie is known to have murdered at least one person.'} email={'maggie@simpson.com'} />
+              <ProfileCard pic={marge} name={'Marge Simpson'} desc={'Marge is a role model.'} email={'marge@simpson.com'} />
+              <ProfileCard pic={bart} name={'Bart Simpson'} desc={'Bart is the eternal prankster.'} email={'bart@simpson.com'} />
             </div>
           </div>
       </section>
